eb966dac5b5152c448ae572229daf7d47a6985dc
ws <wohshon@gmail.com>
2019-06-03 09:55:17 +0800

    return output

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,15 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
 	var payload = req.query.msg;
 	console.log(payload);
+	res.send("received </br>" + payload);
 });
 
 /* POST home page. */
 router.post('/', function(req, res, next) {
 	var payload = req.body;
 	console.log(payload);
+
+	res.send("received </br>" + Object.keys(payload));
 });
 
 module.exports = router;
